0d37046f4ab050ca596b9522b36c332f7b5ae5fd
johnnyfuchs <johnnyfuchs@gmail.com>
2014-12-11 16:55:03 -0800

    WHI example

--- Example/WHICollectionViewTileLayout/WHIAppDelegate.m
+++ Example/WHICollectionViewTileLayout/WHIAppDelegate.m
@@ -7,12 +7,15 @@
 //
 
 #import "WHIAppDelegate.h"
+#import "WHIViewController.h"
 
 @implementation WHIAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [[WHIViewController alloc] init];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 							
